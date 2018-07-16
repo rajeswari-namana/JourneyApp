d43ca09f4d55277e919c206b3681169b3d7ea840
Rajeswari Devi Namana <raji.namana@gmail.com>
2018-07-16 18:37:49 +0530

    Add files via upload

--- Source/app.js
+++ Source/app.js
@@ -4,22 +4,40 @@
 
 var express=require('express');
 var app=express();
+var bodyparser=require("body-parser");
+
+app.use(bodyparser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
+
+var photographs=[
+    {name:"India Gate", image:"https://pixabay.com/get/ea30b3092af3063ed1584d05fb1d4e97e07ee3d21cac104496f1c67aa2ebb1b9_340.jpg"},
+    {name:"Taj Mahal", image:"https://pixabay.com/get/e835b20f20fd053ed1584d05fb1d4e97e07ee3d21cac104496f1c67aa2e4b4b8_340.jpg"},
+    {name:"Red Fort", image:"https://pixabay.com/get/eb37b40b2ff4053ed1584d05fb1d4e97e07ee3d21cac104496f1c67aa1edb6b9_340.jpg"}
+];
 
 app.get("/", function(req,res){
    res.render("landing");
 });
 
 app.get("/photographs", function(req,res){
-    var photographs=[
-        {name:"India Gate", image:"https://pixabay.com/get/ea30b3092af3063ed1584d05fb1d4e97e07ee3d21cac104496f1c67aa2ebb1b9_340.jpg"},
-        {name:"Taj Mahal", image:"https://pixabay.com/get/e835b20f20fd053ed1584d05fb1d4e97e07ee3d21cac104496f1c67aa2e4b4b8_340.jpg"},
-        {name:"Red Fort", image:"https://pixabay.com/get/eb37b40b2ff4053ed1584d05fb1d4e97e07ee3d21cac104496f1c67aa1edb6b9_340.jpg"}
-    ];
+
     res.render("photographs",{photographs:photographs});
 });
 
+app.post("/photographs",function (req,res) {
+   //get data from form and add to photographs array
+    var place=req.body.place;
+    var image=req.body.image;
+    var newplace={name:place, image:image};
+    photographs.push(newplace);
+    //redirect back to photographs
+    res.redirect("photographs")
+});
+
+app.get("/photographs/new",function(req,res){
+        res.render("new.ejs")
+});
 
 app.listen(3000,function(){
    console.log("The Journey Server has started :)!!");
