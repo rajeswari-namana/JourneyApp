16bdc5912111d43618add877de149850644ce2ed
Rajeswari Devi Namana <raji.namana@gmail.com>
2018-07-31 09:06:15 +0530

    RESTful routes

--- Source/app.js
+++ Source/app.js
@@ -2,32 +2,35 @@
  * Created by rajin on 7/16/2018.
  */
 
+//imports
 var express=require('express'),
     app=express(),
     bodyparser=require("body-parser"),
     mongoose=require("mongoose"),
     mongodb=require("mongodb");
 
+//connections & setup
 mongoose.connect('mongodb://localhost:27017/Journey_DB', { useNewUrlParser: true });
-
 app.use(bodyparser.urlencoded({extended:true}));
-
 app.set("view engine", "ejs");
 
 // Schema Setup
-
 var placeSchema= new mongoose.Schema({
    name:String,
-   image:String
+   image:String,
+   description:String
 });
-
 var Photograph=mongoose.model("Place",placeSchema);
 
 
+
+
+//routes
 app.get("/", function(req,res){
    res.render("landing");
 });
 
+//Index Route - Displays all different places
 app.get("/photographs", function(req,res){
     //get all places from db
     Photograph.find({},function (err,allPlaces) {
@@ -35,12 +38,12 @@ app.get("/photographs", function(req,res){
             console.log(err)
         }
         else {
-            res.render("photographs",{photographs:allPlaces});
+            res.render("index",{photographs:allPlaces});
         }
     })
-
 });
 
+//Create Route - Adds new pla ce to database
 app.post("/photographs",function (req,res) {
     //get data from form and add to photographs array
     var place = req.body.place;
@@ -53,18 +56,24 @@ app.post("/photographs",function (req,res) {
         }
         else{
             //redirect back to photographs
-            res.redirect("photographs")
+            res.redirect("index")
         }
     })
 });
 
-   
-
-
+//New Route - Displays a form to make a new place
 app.get("/photographs/new",function(req,res){
         res.render("new.ejs")
 });
 
+//Show Route - shows info about one particular place
+app.get("/photographs/:id",function(req,res){
+    //find place with provided id
+    //render show template with that place
+   res.render("show")
+});
+
+//Starting server
 app.listen(3000,function(){
    console.log("The Journey Server has started :)!!");
 });
